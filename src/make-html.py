29be820f1daea59951ba984210ece0fcf7e9ea61
ken pu <kenpuca@gmail.com>
2014-12-16 02:06:16 -0500

    styling to main.html

--- src/make-html.py
+++ src/make-html.py
@@ -11,6 +11,37 @@ code = "".join(lines)
 
 with open("main.html", "w") as out:
     print >>out, """
-    <a href="javascript:void (function(){%s})()">Bookmark Me</a>;
+    <html>
+    <head>
+    <style>
+    body {
+        position: fixed;
+        display: table;
+        width: 100%%;
+        height: 100%%;
+    }
+    .inner {
+        display: table-cell;
+        vertical-align: middle;
+        width: 100%%;
+        height: 100%%;
+        text-align: center;
+    }
+    a {
+        font-family: "Helvetica";
+        font-size: 32pt;
+    }
+
+    </style>
+    </head>
+    <body>
+        <div class="inner">
+        <a title="Bookmark This."
+           href="javascript:void (function(){%s})()">
+        Mycampus Marker
+        </a>
+        </div>
+    </body>
+    </html>
     """ % urllib.quote(code)
 
